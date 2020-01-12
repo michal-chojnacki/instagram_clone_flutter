84437da55e97c5344c99ae3c6019f592a69342c0
Michal Chojnacki <chojnackimichal@hotmail.com>
2020-01-12 18:27:39 +0100

    Rough styling of content item view

--- lib/features/content/presentation/widget/content_item.dart
+++ lib/features/content/presentation/widget/content_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/features/content/domain/model/content.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 class ContentItem extends StatelessWidget {
   final Content _content;
@@ -14,13 +15,52 @@ class ContentItem extends StatelessWidget {
           padding: const EdgeInsets.all(8.0),
           child: Column(
             children: <Widget>[
-              Image.network(
-                _content.image.url
+              Row(
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(
+                      _content.owner.avatar.url,
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 2.0),
+                  ),
+                  Text(_content.owner.username,
+                      style: new TextStyle(fontWeight: FontWeight.bold)),
+                ],
               ),
-              Text(_content.description),
+              Image.network(_content.image.url),
+              Container(
+                  margin: const EdgeInsets.symmetric(vertical: 4.0),
+                  alignment: Alignment.centerLeft,
+                  child: RichText(
+                    text: TextSpan(
+                        style: new TextStyle(
+                          fontSize: 14.0,
+                          color: Colors.black,
+                        ),
+                        children: <TextSpan>[
+                          TextSpan(
+                              text: _content.owner.username,
+                              style:
+                                  new TextStyle(fontWeight: FontWeight.bold)),
+                          TextSpan(text: ' '),
+                          TextSpan(text: _content.description),
+                        ]),
+                  )),
+              Container(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  timeago.format(DateTime.fromMillisecondsSinceEpoch(
+                      _content.publicationTimestamp)),
+                  style: new TextStyle(
+                    fontSize: 10.0,
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
             ],
-          )
-      ),
+          )),
     );
   }
 }

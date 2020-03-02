6253331f0afd112bbe5565adc7c1f6e9083eab08
Michal Chojnacki <michal.chojnacki@intive.com>
2020-03-02 16:16:31 +0100

    Implements profile view

--- lib/features/profile/presentation/profile_widget.dart
+++ lib/features/profile/presentation/profile_widget.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/features/content/domain/model/image.dart'
+    as model;
+import 'package:instagram_clone/features/content/domain/model/user.dart';
+import 'package:instagram_clone/features/profile/presentation/profile_info_widget.dart';
 
 class ProfileWidget extends StatefulWidget {
-
   @override
   _ProfileWidgetState createState() => _ProfileWidgetState();
 }
@@ -10,7 +13,27 @@ class _ProfileWidgetState extends State<ProfileWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
-       child: Center(child: Text('Profile')),
+      child: Column(
+        children: <Widget>[
+          Container(
+              margin: EdgeInsets.all(16.0),
+              child: ProfileInfoWidget(
+                user: User.create(
+                    username: "elo",
+                    avatar: model.Image.create(
+                        url:
+                            "https://upload.wikimedia.org/wikipedia/commons/1/16/Zenon_Martyniuk_%28member_of_Polish_band_Akcent%29_2018_.jpg")),
+                onSelectAvatar: () => {
+                  print('onSelectAvatar')
+                },
+                onEditProfile: () => {
+                  print('onEditProfile')
+
+                },
+              )),
+          Expanded(child: Center(child: Text('Profile'))),
+        ],
+      ),
     );
   }
 }

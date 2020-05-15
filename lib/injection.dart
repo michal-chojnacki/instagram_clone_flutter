29faaaae6b06331649aac6a468e608f17bbc1925
Michal Chojnacki <michal.chojnacki@intive.com>
2020-05-15 21:07:38 +0200

    Configure Charles

--- lib/injection.dart
+++ lib/injection.dart
@@ -21,17 +21,35 @@ Future<void> configureInjection({@required Environment environment}) async {
 abstract class RegisterModule {
   // @lazySingleton
   @RegisterAs(Client)
-  IOClient get create => IOClient(
-    HttpClient()..connectionTimeout = const Duration(seconds: 5),
-  );
+  IOClient get create => IOClient(HttpClient()
+    ..connectionTimeout = const Duration(seconds: 5)
+    ..proxy = false);
 
   // @lazySingleton
   ChopperClient get chopperClient => ChopperClient(
-        baseUrl: "http://192.168.1.12:8080",
+        baseUrl: "http://192.168.1.7:8080",
         converter: GetIt.I<BuiltValueConverter>(),
         client: GetIt.I<Client>(),
       );
 
   @preResolve
   Future<SharedPreferences> get prefs => SharedPreferences.getInstance();
+}
+
+extension HttpClientProxy on HttpClient {
+  String get proxyIp => '192.168.1.9';
+
+  String get proxyAddress =>
+      Platform.isAndroid ? '$proxyIp:8888' : 'localhost:8888';
+
+  set proxy(bool withProxy) {
+    if (!withProxy) {
+      return;
+    }
+    findProxy = (uri) {
+      return "PROXY $proxyAddress;";
+    };
+    badCertificateCallback =
+        ((X509Certificate cert, String host, int port) => Platform.isAndroid);
+  }
 }
